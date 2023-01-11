355eba06df8d0554dd22ab66b5de5c119b43d15d
Sasha <moneyharassment@rambler.ru>
2023-01-11 23:28:45 +0300

    go:docs

--- volta_ctx.go
+++ volta_ctx.go
@@ -10,6 +10,7 @@ func (c *Ctx) Next() error {
 	return ErrorNext
 }
 
+// Header returns the value of the header field with the given name.
 func (c *Ctx) Header(key, def string) string {
 	if val := c.Request.Header.Get(key); val != "" {
 		return val
@@ -18,16 +19,19 @@ func (c *Ctx) Header(key, def string) string {
 	return def
 }
 
+// SetHeader sets the header with the given key and value.
 func (c *Ctx) SetHeader(key, val string) *Ctx {
 	c.Response.Header().Set(key, val)
 	return c
 }
 
+// Status sets the status code.
 func (c *Ctx) Status(status Status) *Ctx {
 	c.Response.WriteHeader(int(status))
 	return c
 }
 
+// Redirect redirects to the given url.
 func (c *Ctx) Redirect(url string) error {
 	c.Response.Header().Set("Location", url)
 	c.Response.WriteHeader(int(StatusSeeOther))
@@ -35,6 +39,7 @@ func (c *Ctx) Redirect(url string) error {
 	return nil
 }
 
+// RedirectStatus redirects to the given url with the given status.
 func (c *Ctx) RedirectStatus(url string, status int) error {
 	c.Response.Header().Set("Location", url)
 	c.Response.WriteHeader(status)
@@ -42,18 +47,21 @@ func (c *Ctx) RedirectStatus(url string, status int) error {
 	return nil
 }
 
+// SendString sends a string response.
 func (c *Ctx) SendString(msg string) error {
 	c.Response.Write([]byte(msg))
 
 	return nil
 }
 
+// Send sends a byte array response.
 func (c *Ctx) Send(msg []byte) error {
 	c.Response.Write(msg)
 
 	return nil
 }
 
+// SendJSON sends a JSON response.
 func (c *Ctx) SendJSON(msg Map) error {
 	if c.jsonMarshaler == nil {
 		return ErrorNoJSONMarshaler
@@ -70,6 +78,7 @@ func (c *Ctx) SendJSON(msg Map) error {
 	return nil
 }
 
+// Form returns the value of the form field with the given name.
 func (c *Ctx) Form(key, def string) string {
 	if val := c.Request.FormValue(key); val != "" {
 		return val
@@ -78,6 +87,7 @@ func (c *Ctx) Form(key, def string) string {
 	return def
 }
 
+// Form returns the value of the form file field with the given name.
 func (c *Ctx) FormFile(key string) (multipart.File, error) {
 	file, _, err := c.Request.FormFile(key)
 	if err != nil {
@@ -87,6 +97,7 @@ func (c *Ctx) FormFile(key string) (multipart.File, error) {
 	return file, nil
 }
 
+// SaveFile saves the file to the given path.
 func (c *Ctx) SaveFile(file multipart.File, path string) error {
 	out, err := os.Create(path)
 	if err != nil {
@@ -103,6 +114,7 @@ func (c *Ctx) SaveFile(file multipart.File, path string) error {
 	return nil
 }
 
+// Query returns the value of the query field with the given name.
 func (c *Ctx) Query(key, def string) string {
 	if val := c.Request.URL.Query().Get(key); val != "" {
 		return val
@@ -111,6 +123,7 @@ func (c *Ctx) Query(key, def string) string {
 	return def
 }
 
+// Param returns the value of the param field with the given name.
 func (c *Ctx) Param(key, def string) string {
 	if val := c.ps.ByName(key); val != "" {
 		return val
